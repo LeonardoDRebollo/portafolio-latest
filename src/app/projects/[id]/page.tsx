7deeb8be2578d386bb6d598f3c6da1510791e80b
LeonardoDRebollo <leonardod.rebollo@gmail.com>
2025-01-24 08:40:14 -0500

    id

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -12,8 +12,10 @@ import { IconButton } from "@mui/material";
 import petmania from "../../../../public/images/Petmania/petmania-1.png";
 import Image from "next/image";
 
-export default function ProjectPage({params,}: {params: { id: any };}) {
+export default function ProjectPage({ params }: any) {
   const router = useRouter();
+  const { id } = params;
+  
   const [stars, setStars] = useState<Star[]>([]);
 
   useEffect(() => {
@@ -55,7 +57,7 @@ export default function ProjectPage({params,}: {params: { id: any };}) {
         </section>
         <section className={styles.project_section}>
           <div className={styles.description_project}>
-            <h2>Proyecto</h2>
+            <h2>Proyecto {id}</h2>
             <p>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including 
             versions of Lorem Ipsum.
